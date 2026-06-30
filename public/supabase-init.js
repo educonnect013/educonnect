94d8659dd3658433ba54943c0947abc2bdeb4da4
Khutso Mokoena <sataxiconnect@gmail.com>
2026-06-30 14:57:36 +0200

    Initial EduConnect project setup

---
+++ public/supabase-init.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://cwyxniubvpfsfucnwabr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3eXhuaXVidnBmc2Z1Y253YWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTU3NDgsImV4cCI6MjA5ODM5MTc0OH0.Q8oYcObC5CpSUJQcPLbqcPTBvvlCEVxlOCVr3vBsXDs";
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
